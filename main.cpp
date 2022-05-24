ea5306bab049baef1a4d14037a8db96c8f72c63d
SahilJoseph001 <sahil.joseph.0.0.1@gmail.com>
2022-05-24 18:16:27 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// *** Coded by Sahil Joseph ***
+# include <iostream>
+
+int main() {
+
+	while (true) {
+
+		int a;
+
+		std::cout << "SahilJoseph" << std::endl;
+
+		std::cin >> a;
+
+	}
+
+	return 0;
+}
